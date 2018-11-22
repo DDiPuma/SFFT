22058d6b3673a66ac77fbe17c61d1f5ced4bce58
Dominic DiPuma <dominic.dipuma@rutgers.edu>
2018-11-21 22:41:09 -0500

    Add main m-file which should run and plot, as well as 'prove' accuracy of SFFT

---
+++ main.m
@@ -0,0 +1,19 @@
+%% Generate Test Data
+
+% TODO - generate series of various lengths, fixed k
+
+% TODO - generate series of various ks, fixed length
+
+% Ensure that lengths are 2^m
+
+%% Run SFFT and Builtin FFT
+
+% TODO - time them
+
+%% Plot runtime performance
+
+% TODO - plot comparisons
+
+%% Verify accuracy
+
+% TODO - ensure that FFT and SFFT outputs match
