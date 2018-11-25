c20bcfc53a0b448ace5c2407ba31f7ad97736012
Brendan <abae97@gmail.com>
2018-11-25 14:53:45 -0500

    Filled out window_experiment2.m

---
+++ experiments/window_experiment2.m
@@ -0,0 +1,39 @@
+B=64;
+N=2^10; alpha=B*sqrt(log(N));
+G_gaussian = gausswin(N,alpha); % alpha is recipricol of standard deviation
+G_cheby = chebwin(N,alpha); % alpha determines the dielobe attenuation
+figure;
+stem(G_gaussian)
+title('Ggaussian')
+figure;
+stem(G_cheby)
+title('Gcheby')
+figure;
+stem(abs(fftshift(fft(G_gaussian))))
+title('Ghatgaussian')
+figure;
+stem(abs(fftshift(fft(G_cheby))))
+title('Ghatcheby')
+
+
+H_boxcar = boxcar(N/B);
+figure;
+stem(H_boxcar)
+title('Hboxcar')
+
+
+Fhat_gaussian = conv(fft(G_gaussian), H_boxcar);
+Fhat_cheby = conv(fft(G_cheby), H_boxcar);
+figure;
+stem(abs(Fhat_gaussian))
+title('Fhatgaussian')
+figure;
+stem(abs(Fhat_cheby))
+title('Fhatcheby')
+
+figure;
+stem(abs(ifft(Fhat_gaussian)))
+title('Fgaussian')
+figure;
+stem(abs(ifft(Fhat_cheby)))
+title('Fcheby')
